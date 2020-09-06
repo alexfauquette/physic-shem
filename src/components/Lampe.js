cfaedddd81d7fdbafd9168c44588c15915d3cd86
Alexandre Fauquette <alex.fauquette@gmail.com>
2020-09-06 15:02:24 +0200

    install react-redux to deal with data

--- src/components/Lampe.js
+++ src/components/Lampe.js
@@ -1,14 +1,30 @@
 import React from "react";
+import { connect } from "react-redux";
 
 const style = {
   fill: "transparent",
   stroke: "black",
   strokeWidth: 2,
 };
-const Lampe = ({ x, y, ...props }) => (
-  <g style={{ transform: `translate(${x}px,${y}px)` }} {...props}>
+
+const mapStateToProps = (state) => ({ x: state.x, y: state.y });
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    // dispatching plain actions
+    incrementX: () => dispatch({ type: "INCREMENT_X" }),
+    decrementY: () => dispatch({ type: "INCREMENT_Y" }),
+  };
+};
+
+const Lampe = ({ x, y, incrementX, decrementY, ...props }) => (
+  <g
+    style={{ transform: `translate(${x}px,${y}px)` }}
+    {...props}
+    onMouseOver={incrementX}
+  >
     <circle cx={0} cy={0} r={10} style={style} />
     <path d={`M -7 -7 L 7 7 M -7 7 L 7 -7`} style={style} />
   </g>
 );
-export default Lampe;
+export default connect(mapStateToProps, mapDispatchToProps)(Lampe);
