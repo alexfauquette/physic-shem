403265d136580dbde20bf724447d01b57fa942b7
Alexandre Fauquette <alex.fauquette@gmail.com>
2020-09-14 22:50:56 +0200

    allow to clik on handlers

--- src/components/Lampe.js
+++ src/components/Lampe.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-
-const style = {
-  fill: "transparent",
-  strokeWidth: 2,
-};
+import "./style.scss";
 
 // If id => it's from scene
 // If no id => it's from adding
@@ -12,29 +8,30 @@ const mapStateToProps = (state, props) => {
   return props.id ? state.scene[props.id] : {};
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = () => {
   return {};
 };
 
-const Lampe = ({ x, y, selected, ...props }) =>
+const Lampe = ({ x, y, selected, showHandles, ...props }) =>
   x && y ? (
     <g
       style={{
         transform: `translate(${x}px,${y}px)`,
-        stroke: selected ? "red" : "black",
       }}
+      className={`component ${selected ? "red" : "black"}`}
       {...props}
     >
-      <circle cx={0} cy={0} r={10} style={style} />
-      <path d={`M -7 -7 L 7 7 M -7 7 L 7 -7`} style={style} />
-      <path d={`M -20 0 L -10 0`} style={style} />
-      <path d={`M 20 0 L 10 0`} style={style} />
-      {selected && (
-        <>
-          <circle cx={-20} cy={0} r={5} style={style} />
-          <circle cx={20} cy={0} r={5} style={style} />
-        </>
-      )}
+      <circle cx={0} cy={0} r={10} />
+      <path d={`M -7 -7 L 7 7 M -7 7 L 7 -7`} />
+      <path d={`M -20 0 L -10 0`} />
+      <path d={`M 20 0 L 10 0`} />
+      <circle cx={-20} cy={0} r={5} className="handle" />
+      <circle cx={20} cy={0} r={5} className="handle" />
+      <path
+        d={`M -20 0 L 20 0`}
+        style={{ stroke: "transparent", strokeWidth: "10" }}
+      />
+      {/* helper for selection */}
     </g>
   ) : null;
 export default connect(mapStateToProps, mapDispatchToProps)(Lampe);
