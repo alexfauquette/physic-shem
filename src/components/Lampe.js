e0f61c6d0406b4861f600daaa658a60f19effd63
Alexandre Fauquette <alex.fauquette@gmail.com>
2020-11-20 21:50:12 +0100

    refacto lamp

--- src/components/Lampe.js
+++ src/components/Lampe.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { connect } from "react-redux";
-// import { MODE_LINK, MODE_SELECT } from "../redux/store";
 import "./style.scss";
 
+const R = 60;
+const r = ((Math.sqrt(2) * R) / 2).toFixed(3);
 // If id => it's from scene
 // If no id => it's from adding
 const mapStateToProps = (state, props) => {
@@ -37,7 +38,7 @@ const Lampe = ({
   const { x: xTo, y: yTo } = toCoords;
 
   const d = Math.sqrt((xFrom - xTo) ** 2 + (yFrom - yTo) ** 2);
-  const ratio = 1 / 2 - 10 / d;
+  const ratio = 1 / 2 - R / d;
   return (
     <g className={`component ${selected ? "red" : "black"}`}>
       <g {...props}>
@@ -48,8 +49,8 @@ const Lampe = ({
             }px)`,
           }}
         >
-          <circle cx={0} cy={0} r={10} />
-          <path d={`M -7 -7 L 7 7 M -7 7 L 7 -7`} />
+          <circle cx={0} cy={0} r={R} />
+          <path d={`M -${r} -${r} L ${r} ${r} M -${r} ${r} L ${r} -${r}`} />
         </g>
         <path
           d={`M ${xFrom} ${yFrom} L ${xFrom + ratio * (xTo - xFrom)} ${
@@ -61,36 +62,7 @@ const Lampe = ({
             yTo + ratio * (yFrom - yTo)
           }`}
         />
-        {/*
-        path to help hover event
-        <path
-          d={`M -25 0 L 25 0`}
-          style={{ stroke: "transparent", strokeWidth: "10" }}
-        /> */}
       </g>
-      {/* helper for selection */}
-      {/* <circle
-        cx={-20}
-        cy={0}
-        r={5}
-        className="handle"
-        onClick={
-          (mode === MODE_SELECT && startLinking(id, x - 20, y)) ||
-          (mode === MODE_LINK && stopLinking(id, x - 20, y)) ||
-          null
-        }
-      />
-      <circle
-        cx={20}
-        cy={0}
-        r={5}
-        className="handle"
-        onClick={
-          (mode === MODE_SELECT && startLinking(id, x + 20, y)) ||
-          (mode === MODE_LINK && stopLinking(id, x + 20, y)) ||
-          null
-        }
-      /> */}
     </g>
   );
 };
