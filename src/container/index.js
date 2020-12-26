29950c08543acd85e73a0c0cd0e6be2b999dd68f
Alexandre Fauquette <alex.fauquette@gmail.com>
2020-12-26 22:12:54 +0100

    refacto toolbar

--- src/container/index.js
+++ src/container/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
 import components from "../components";
+import ToolBar from "./toolbar";
+
 import {
   updatePosition,
   stopDragging,
@@ -10,12 +12,7 @@ import {
   nextStepOfElementCreation,
   startRectangleSelection,
   stopRectangleSelection,
-  setZoom,
   startMovePaper,
-  setModeMovePaper,
-  startSelect,
-  stackSelectedAnchors,
-  updateMagnetOption,
 } from "../redux/actions";
 import {
   MODE_DRAG,
@@ -32,15 +29,6 @@ import Components from "./components";
 import Anchors from "./anchors";
 import Magnets from "./magnets";
 
-import SvgIcon from "@material-ui/core/SvgIcon";
-import IconButton from "@material-ui/core/IconButton";
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
-
-import ControlCameraIcon from "@material-ui/icons/ControlCamera";
-import ZoomInIcon from "@material-ui/icons/ZoomIn";
-import ZoomOutIcon from "@material-ui/icons/ZoomOut";
-
 const mapDispatchToProps = (dispatch) => {
   return {
     updatePosition: (x, y, shiftPress) =>
@@ -54,14 +42,7 @@ const mapDispatchToProps = (dispatch) => {
     nextStepOfElementCreation: () => dispatch(nextStepOfElementCreation()),
     startRectangleSelection: (x, y) => dispatch(startRectangleSelection(x, y)),
     stopRectangleSelection: () => dispatch(stopRectangleSelection()),
-    setZoom: (zoom) => dispatch(setZoom(zoom)),
     startMovePaper: (x, y) => dispatch(startMovePaper(x, y)),
-    setModeMovePaper: () => dispatch(setModeMovePaper()),
-    startSelect: () => dispatch(startSelect()),
-    stackSelectedAnchors: (direction) =>
-      dispatch(stackSelectedAnchors(direction)),
-    updateMagnetOption: (optionName, optionValue = null) =>
-      dispatch(updateMagnetOption(optionName, optionValue)),
   };
 };
 const mapStateToProps = (state) => {
@@ -71,7 +52,6 @@ const mapStateToProps = (state) => {
     newNode: state.newNode,
     rectangleSelection: state.rectangleSelection,
     displayOptions: state.displayOptions,
-    magnetsOptions: state.magnetsOptions,
     isGridAttracting: state.magnetsOptions.isGridAttracting,
     gridSpace: state.magnetsOptions.gridSpace,
   };
@@ -94,20 +74,13 @@ const Container = ({
   nextStepOfElementCreation,
   startRectangleSelection,
   stopRectangleSelection,
-  setZoom,
   startMovePaper,
-  setModeMovePaper,
-  startSelect,
-  stackSelectedAnchors,
-  updateMagnetOption,
-  magnetsOptions,
 }) => {
   const {
     x: SVG_X,
     y: SVG_Y,
     width: SVG_WIDTH,
     height: SVG_HEIGHT,
-    zoom,
   } = displayOptions;
 
   const followMouse = (event) => {
@@ -182,134 +155,7 @@ const Container = ({
 
   return (
     <>
-      {/* TODO create a clean toolbar */}
-      <IconButton
-        color={mode === MODE_MOVE_PAPER ? "secondary" : "default"}
-        onMouseDown={(event) => {
-          event.stopPropagation();
-          if (mode === MODE_MOVE_PAPER) {
-            startSelect();
-          } else {
-            setModeMovePaper();
-          }
-        }}
-      >
-        <ControlCameraIcon />
-      </IconButton>
-      <IconButton
-        disabled={zoom < 0.2}
-        onMouseDown={(event) => {
-          event.stopPropagation();
-          setZoom(zoom / 2);
-        }}
-      >
-        <ZoomOutIcon />
-      </IconButton>
-      {zoom}
-      <IconButton
-        disabled={zoom > 2}
-        onMouseDown={(event) => {
-          event.stopPropagation();
-          setZoom(zoom * 2);
-        }}
-      >
-        <ZoomInIcon />
-      </IconButton>
-      |
-      <IconButton
-        onMouseDown={(event) => {
-          event.stopPropagation();
-          stackSelectedAnchors("L");
-        }}
-      >
-        <SvgIcon>
-          <path d="M22 13V19H6V13H22M6 5V11H16V5H6M2 2V22H4V2H2" />
-        </SvgIcon>
-      </IconButton>
-      <IconButton
-        onMouseDown={(event) => {
-          event.stopPropagation();
-          stackSelectedAnchors("R");
-        }}
-      >
-        <SvgIcon>
-          <path d="M18 13V19H2V13H18M8 5V11H18V5H8M20 2V22H22V2H20Z" />
-        </SvgIcon>
-      </IconButton>
-      <IconButton
-        onMouseDown={(event) => {
-          event.stopPropagation();
-          stackSelectedAnchors("D");
-        }}
-      >
-        <SvgIcon>
-          <path d="M11 22H5V6H11V22M19 6H13V16H19V6M22 2H2V4H22V2Z" />
-        </SvgIcon>
-      </IconButton>
-      <IconButton
-        onMouseDown={(event) => {
-          event.stopPropagation();
-          stackSelectedAnchors("U");
-        }}
-      >
-        <SvgIcon>
-          <path d="M11 18H5V2H11V18M19 8H13V18H19V8M22 20H2V22H22V20Z" />
-        </SvgIcon>
-      </IconButton>
-      |
-      <IconButton
-        color={magnetsOptions.isGridAttracting ? "secondary" : "default"}
-        onMouseDown={(event) => {
-          event.stopPropagation();
-          updateMagnetOption("isGridAttracting");
-        }}
-      >
-        <SvgIcon>
-          <path d="M0 6v1h24v-1ZM0 12v1h24v-1ZM0 18v1h24v-1Z" />
-          <path d="M4 0h1v24h-1ZM11 0h1v24h-1ZM18 0h1v24h-1Z" />
-          <circle cx={18.5} cy={18.5} r={3} />
-        </SvgIcon>
-      </IconButton>
-      <Select
-        value={magnetsOptions.gridSpace}
-        onChange={(event) =>
-          updateMagnetOption("gridSpace", event.target.value)
-        }
-      >
-        <MenuItem value={0.5}>0.5</MenuItem>
-        <MenuItem value={1}>1</MenuItem>
-        <MenuItem value={2}>2</MenuItem>
-      </Select>
-      <IconButton
-        color={
-          magnetsOptions.isPathCoordinatesAttracting ? "secondary" : "default"
-        }
-        onMouseDown={(event) => {
-          event.stopPropagation();
-          updateMagnetOption("isPathCoordinatesAttracting");
-        }}
-      >
-        <SvgIcon>
-          <path d="M4 11.5h20v1h-20Z" />
-
-          <circle cx={2} cy={12} r={2} />
-          <circle cx={22} cy={12} r={2} />
-        </SvgIcon>
-      </IconButton>
-      <IconButton
-        color={magnetsOptions.isNodeAnchorsAttracting ? "secondary" : "default"}
-        onMouseDown={(event) => {
-          event.stopPropagation();
-          updateMagnetOption("isNodeAnchorsAttracting");
-        }}
-      >
-        <SvgIcon>
-          <path d="M4 11.5h20v10h-20v-10M8 13h10v5h-10Z" />
-
-          <circle cx={2} cy={12} r={2} />
-          <circle cx={22} cy={12} r={2} />
-        </SvgIcon>
-      </IconButton>
+      <ToolBar />
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox={`${SVG_X} ${SVG_Y} ${SVG_WIDTH} ${SVG_HEIGHT}`}
@@ -421,20 +267,6 @@ const Container = ({
           />
         )}
       </svg>
-      {/* <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox={`0 0 210 297`}
-        style={{
-          width: "210px",
-          height: "297px",
-          background: "gray",
-          position: "absolute",
-          bottom: "10px",
-          left: "200px",
-        }}
-      >
-        <rect x="10" y="20" width="20" height="30" style={{ fill: "white" }} />
-      </svg> */}
     </>
   );
 };
---
+++ src/container/toolbar.js
@@ -0,0 +1,184 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import {
+  setZoom,
+  setModeMovePaper,
+  startSelect,
+  stackSelectedAnchors,
+  updateMagnetOption,
+} from "../redux/actions";
+import { MODE_MOVE_PAPER } from "../redux/store/interactionModes";
+
+import SvgIcon from "@material-ui/core/SvgIcon";
+import IconButton from "@material-ui/core/IconButton";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+
+import ControlCameraIcon from "@material-ui/icons/ControlCamera";
+import ZoomInIcon from "@material-ui/icons/ZoomIn";
+import ZoomOutIcon from "@material-ui/icons/ZoomOut";
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setZoom: (zoom) => dispatch(setZoom(zoom)),
+    setModeMovePaper: () => dispatch(setModeMovePaper()),
+    startSelect: () => dispatch(startSelect()),
+    stackSelectedAnchors: (direction) =>
+      dispatch(stackSelectedAnchors(direction)),
+    updateMagnetOption: (optionName, optionValue = null) =>
+      dispatch(updateMagnetOption(optionName, optionValue)),
+  };
+};
+const mapStateToProps = (state) => {
+  return {
+    mode: state.mode,
+    zoom: state.displayOptions.zoom,
+    magnetsOptions: state.magnetsOptions,
+  };
+};
+
+const ToolBar = ({
+  mode,
+  zoom,
+  magnetsOptions,
+  setZoom,
+  setModeMovePaper,
+  stackSelectedAnchors,
+  updateMagnetOption,
+}) => {
+  return (
+    <>
+      {/* TODO create a clean toolbar */}
+      <IconButton
+        color={mode === MODE_MOVE_PAPER ? "secondary" : "default"}
+        onMouseDown={(event) => {
+          event.stopPropagation();
+          if (mode === MODE_MOVE_PAPER) {
+            startSelect();
+          } else {
+            setModeMovePaper();
+          }
+        }}
+      >
+        <ControlCameraIcon />
+      </IconButton>
+      <IconButton
+        disabled={zoom < 0.2}
+        onMouseDown={(event) => {
+          event.stopPropagation();
+          setZoom(zoom / 2);
+        }}
+      >
+        <ZoomOutIcon />
+      </IconButton>
+      {zoom}
+      <IconButton
+        disabled={zoom > 2}
+        onMouseDown={(event) => {
+          event.stopPropagation();
+          setZoom(zoom * 2);
+        }}
+      >
+        <ZoomInIcon />
+      </IconButton>
+      |
+      <IconButton
+        onMouseDown={(event) => {
+          event.stopPropagation();
+          stackSelectedAnchors("L");
+        }}
+      >
+        <SvgIcon>
+          <path d="M22 13V19H6V13H22M6 5V11H16V5H6M2 2V22H4V2H2" />
+        </SvgIcon>
+      </IconButton>
+      <IconButton
+        onMouseDown={(event) => {
+          event.stopPropagation();
+          stackSelectedAnchors("R");
+        }}
+      >
+        <SvgIcon>
+          <path d="M18 13V19H2V13H18M8 5V11H18V5H8M20 2V22H22V2H20Z" />
+        </SvgIcon>
+      </IconButton>
+      <IconButton
+        onMouseDown={(event) => {
+          event.stopPropagation();
+          stackSelectedAnchors("U");
+        }}
+      >
+        <SvgIcon>
+          <path d="M11 22H5V6H11V22M19 6H13V16H19V6M22 2H2V4H22V2Z" />
+        </SvgIcon>
+      </IconButton>
+      <IconButton
+        onMouseDown={(event) => {
+          event.stopPropagation();
+          stackSelectedAnchors("D");
+        }}
+      >
+        <SvgIcon>
+          <path d="M11 18H5V2H11V18M19 8H13V18H19V8M22 20H2V22H22V20Z" />
+        </SvgIcon>
+      </IconButton>
+      |
+      <IconButton
+        color={magnetsOptions.isGridAttracting ? "secondary" : "default"}
+        onMouseDown={(event) => {
+          event.stopPropagation();
+          updateMagnetOption("isGridAttracting");
+        }}
+      >
+        <SvgIcon>
+          <path d="M0 6v1h24v-1ZM0 12v1h24v-1ZM0 18v1h24v-1Z" />
+          <path d="M4 0h1v24h-1ZM11 0h1v24h-1ZM18 0h1v24h-1Z" />
+          <circle cx={18.5} cy={18.5} r={3} />
+        </SvgIcon>
+      </IconButton>
+      <Select
+        value={magnetsOptions.gridSpace}
+        onChange={(event) =>
+          updateMagnetOption("gridSpace", event.target.value)
+        }
+      >
+        <MenuItem value={0.5}>0.5</MenuItem>
+        <MenuItem value={1}>1</MenuItem>
+        <MenuItem value={2}>2</MenuItem>
+      </Select>
+      <IconButton
+        color={
+          magnetsOptions.isPathCoordinatesAttracting ? "secondary" : "default"
+        }
+        onMouseDown={(event) => {
+          event.stopPropagation();
+          updateMagnetOption("isPathCoordinatesAttracting");
+        }}
+      >
+        <SvgIcon>
+          <path d="M4 11.5h20v1h-20Z" />
+
+          <circle cx={2} cy={12} r={2} />
+          <circle cx={22} cy={12} r={2} />
+        </SvgIcon>
+      </IconButton>
+      <IconButton
+        color={magnetsOptions.isNodeAnchorsAttracting ? "secondary" : "default"}
+        onMouseDown={(event) => {
+          event.stopPropagation();
+          updateMagnetOption("isNodeAnchorsAttracting");
+        }}
+      >
+        <SvgIcon>
+          <circle id="path858" cx="2" cy="16.5" r="2" />
+          <circle id="path834" cx="2" cy="7.5" r="2" />
+          <circle id="path838" cx="22" cy="12" r="2" />
+          <path d="m 22,11.5 v 1 L 18.5,12.5 7,21 7,17 H 2 V 16 H 7 L 7,8 H 2 v -1 L 7,7 7,3 18.5,11.5 Z" />
+        </SvgIcon>
+      </IconButton>
+    </>
+  );
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ToolBar);
