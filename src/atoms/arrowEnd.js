571a4cf2c77a2a88b7557d6dade66e2abd96d989
Alexandre Fauquette <alex.fauquette@gmail.com>
2021-01-01 19:44:54 +0100

    complet switchs

---
+++ src/atoms/arrowEnd.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+import "components/style.scss";
+import { MULTIPLICATIVE_CONST, rotation } from "utils";
+
+const dx = 0.1;
+const dy = 0.04;
+const ArrowEnd = ({ x, y, angle }) => (
+  <g
+    style={{
+      transform: `translate(${x}px , ${y}px) rotate(${angle}deg)`,
+    }}
+  >
+    <path
+      d={`M 0 0 L ${-dx * MULTIPLICATIVE_CONST} ${
+        dy * MULTIPLICATIVE_CONST
+      } L ${-dx * MULTIPLICATIVE_CONST} ${-dy * MULTIPLICATIVE_CONST} Z`}
+      className="fill"
+    />
+  </g>
+);
+
+export const drawRoughArrowEnd = (rc, x, y, angle, x2, y2, theta) => {
+  const x0 =
+    x +
+    Math.cos((angle / 180) * Math.PI) * x2 -
+    Math.sin((angle / 180) * Math.PI) * y2;
+
+  const y0 =
+    y +
+    Math.sin((angle / 180) * Math.PI) * x2 +
+    Math.cos((-angle / 180) * Math.PI) * y2;
+
+  rc.path(`
+      M ${x0} ${y0}
+      L ${rotation(
+        -angle - theta,
+        x0,
+        y0,
+        -dx * MULTIPLICATIVE_CONST,
+        -dy * MULTIPLICATIVE_CONST
+      )}
+      L ${rotation(
+        -angle - theta,
+        x0,
+        y0,
+        -dx * MULTIPLICATIVE_CONST,
+        dy * MULTIPLICATIVE_CONST
+      )}
+      Z
+  `);
+};
+export default ArrowEnd;
--- src/components/switch.js
+++ src/components/switch.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./style.scss";
-import { MULTIPLICATIVE_CONST, R_LEN, default_path_options } from "utils";
+import {
+  MULTIPLICATIVE_CONST,
+  R_LEN,
+  default_path_options,
+  rotation,
+} from "utils";
 import {
   withPathAttributes,
   getPathAttributes,
   drawLinks,
 } from "./hoc/pathComponents";
+import ArrowEnd, { drawRoughArrowEnd } from "atoms/arrowEnd";
+
 import { drawRoughCurrant } from "atoms/currant";
 
 const height = 0.35;
@@ -19,11 +26,14 @@ const Arrow = ({ x, y, r, theta1, theta2 }) => {
   const y2 = y - r * Math.sin((theta2 / 180) * Math.PI);
 
   return (
-    <path
-      d={`M ${x1} ${y1} A ${r} ${r} 0 0 ${
-        theta1 < theta2 ? 0 : 1
-      } ${x2} ${y2} `}
-    />
+    <>
+      <path
+        d={`M ${x1} ${y1} A ${r} ${r} 0 0 ${
+          theta1 < theta2 ? 0 : 1
+        } ${x2} ${y2} `}
+      />
+      <ArrowEnd x={x2} y={y2} angle={theta1 < theta2 ? 194 : 96} />
+    </>
   );
 };
 
@@ -49,14 +59,19 @@ const Switch = ({ isOpen = true, withArrow = true }) => {
 
   return (
     <path
-      d={`M ${-UNIT_X} 0 L ${0.9 * UNIT_X} ${-UNIT_Y} M ${UNIT_X} 0 L ${
-        0.2 * UNIT_X
-      } 0 L ${0.2 * UNIT_X} ${-UNIT_Y}`}
+      d={`M ${-UNIT_X} 0 L ${0.9 * UNIT_X} ${-UNIT_Y} ${
+        isOpen
+          ? ""
+          : `M ${UNIT_X} 0 L ${0.2 * UNIT_X} 0 L ${0.2 * UNIT_X} ${-UNIT_Y}`
+      }`}
     />
   );
 };
 
 export const roughComponent = (rc, ctx, x0, y0, element) => {
+  const { isOpen, withArrow } = element;
+  const height = withArrow ? 0.35 : 0.3;
+
   const { x, y, angle, ratio } = drawLinks(
     rc,
     ctx,
@@ -69,15 +84,45 @@ export const roughComponent = (rc, ctx, x0, y0, element) => {
 
   drawRoughCurrant(rc, ctx, x0, y0, angle, ratio, element);
 
-  // rc.path(
-  //   `M ${rotation(-angle, x, y, UNIT_X, 0)}
-  //   L ${rotation(-angle, x, y, -UNIT_X, -UNIT_Y)}
-  //   L ${rotation(-angle, x, y, -UNIT_X, UNIT_Y)}
-  //   Z
-  //   M ${rotation(-angle, x, y, UNIT_X, UNIT_Y)}
-  //   L ${rotation(-angle, x, y, UNIT_X, -UNIT_Y)}
-  //   `
-  // );
+  const UNIT_X = 0.5 * width * MULTIPLICATIVE_CONST * R_LEN;
+  const UNIT_Y = 0.5 * height * MULTIPLICATIVE_CONST * R_LEN;
+  if (!withArrow) {
+    rc.path(
+      `M ${rotation(-angle, x, y, -UNIT_X, 0)}
+        L ${rotation(-angle, x, y, 0.9 * UNIT_X, -UNIT_Y)}`
+    );
+    if (!isOpen) {
+      rc.path(
+        `M ${rotation(-angle, x, y, UNIT_X, 0)}
+          L ${rotation(-angle, x, y, 0.2 * UNIT_X, 0)} 
+          L ${rotation(-angle, x, y, 0.2 * UNIT_X, -UNIT_Y)}`
+      );
+    }
+  } else {
+    rc.path(
+      `M ${rotation(-angle, x, y, -UNIT_X, 0)}
+        L ${rotation(-angle, x, y, 0.6 * UNIT_X, -UNIT_Y)}`
+    );
+
+    const cx = -UNIT_X;
+    const r = 1.2 * UNIT_X;
+    const theta1 = isOpen ? -10 : 90;
+    const theta2 = isOpen ? 90 : -20;
+
+    const x1 = cx + r * Math.cos((theta1 / 180) * Math.PI);
+    const y1 = -r * Math.sin((theta1 / 180) * Math.PI);
+
+    const x2 = cx + r * Math.cos((theta2 / 180) * Math.PI);
+    const y2 = -r * Math.sin((theta2 / 180) * Math.PI);
+
+    rc.path(
+      `M ${rotation(-angle, x, y, x1, y1)} A ${r} ${r} 0 0 ${
+        theta1 < theta2 ? 0 : 1
+      } ${rotation(-angle, x, y, x2, y2)}`
+    );
+
+    drawRoughArrowEnd(rc, x, y, angle, x2, y2, theta1 < theta2 ? 194 : 96);
+  }
 };
 
 export const drawer = (element) => {
