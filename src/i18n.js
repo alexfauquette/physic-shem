f7d5239e9b4e439c8a3e5d492aef6dc5b33c4f3f
Alexandre Fauquette <alex.fauquette@gmail.com>
2021-01-25 09:54:24 +0100

    correct typos

--- src/i18n.js
+++ src/i18n.js
@@ -19,7 +19,8 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    fallbackLng: "fr",
+    fallbackLng: "en",
+    lng: ["fr", "en"],
     debug: true,
 
     react: {
